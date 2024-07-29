3e833e7cefe3864688e8e345aff903b2dc32fe43
Caleb Foster <calebalakaifoster@gmail.com>
2024-07-28 22:59:58 -0700

    added wrapping command

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,7 +9,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {},
+  ensure_installed = {'gopls'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
