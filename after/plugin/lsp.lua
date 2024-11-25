0fbe5d5a6b2fa5a7b0e94ef28a58a4c0f2c83bbf
Caleb Foster <calebalakaifoster@gmail.com>
2024-11-24 17:18:34 -0800

    hello

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,7 +9,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'gopls'},
+  ensure_installed = {'gopls','csharp_ls'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
